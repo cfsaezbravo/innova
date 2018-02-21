078aa6d8aa5d395e9bcb128b2b413835aaa0956e
Camila Sáez <cfsaezbravo@gmail.com>
2018-02-21 01:18:03 -0300

    subido humero

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,19 @@
 $('#implantes').click(function(){
 	$('#lista-menu').css("display", "inline-flex");
+	$('#lista-menu-2').css("display", "none");
 });
 
 $('#superior').click(function(){
 	$('#lista-menu-2').css("display", "inline-flex");
+	$('#lista-menu').css("display", "none");
 });
 
 $('#clavicula').click(function(){
 	event.preventDefault()
 	$('#lista-menu-3').css("display", "inline-flex");
+});
+
+$('#humero').click(function(){
+	event.preventDefault()
+	$('#lista-menu-4').css("display", "inline-flex");
 });
