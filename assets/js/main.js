0816847b6466dc3802e101e5a0dcfdc3d1a8663f
Camila Sáez <cfsaezbravo@gmail.com>
2018-02-21 00:58:06 -0300

    modificando extremidades superiores

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,12 @@
 $('#implantes').click(function(){
 	$('#lista-menu').css("display", "inline-flex");
 });
+
+$('#superior').click(function(){
+	$('#lista-menu-2').css("display", "inline-flex");
+});
+
+$('#clavicula').click(function(){
+	event.preventDefault()
+	$('#lista-menu-3').css("display", "inline-flex");
+});
