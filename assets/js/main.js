f82d310343c3b805218e76621c1fdf290882fb07
Camila Sáez <cfsaezbravo@gmail.com>
2018-02-27 00:18:18 -0300

    terminando casi

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,8 +4,6 @@ $('#implantes').click(function(){
 	$('#lista-menu-3').css("display", "none");
 	$('#lista-menu-4').css("display", "none");
 	$('#lista-menu-5').css("display", "none");
-	$('#lista-menu-6').css("display", "none");
-	$('#lista-menu-7').css("display", "none");
 	$('#lista-menu-8').css("display", "none");
 });
 
@@ -16,11 +14,10 @@ $('#superior').click(function(){
 	$('#lista-menu-4').css("display", "none");
 	$('#lista-menu-5').css("display", "none");
 	$('#lista-menu-6').css("display", "none");
-	$('#lista-menu-7').css("display", "none");
 	$('#lista-menu-8').css("display", "none");
 });
 
-$('#inferior').click(function(){
+/*$('#inferior').click(function(){
 	$('#lista-menu-7').css("display", "inline-flex");
 	$('#lista-menu').css("display", "none");
 	$('#lista-menu-2').css("display", "none");
@@ -29,7 +26,7 @@ $('#inferior').click(function(){
 	$('#lista-menu-5').css("display", "none");
 	$('#lista-menu-6').css("display", "none");
 	$('#lista-menu-8').css("display", "none");
-});
+});*/
 
 $('#medicina').click(function(){
 	$('#lista-menu-8').css("display", "inline-flex");
@@ -39,7 +36,6 @@ $('#medicina').click(function(){
 	$('#lista-menu-4').css("display", "none");
 	$('#lista-menu-5').css("display", "none");
 	$('#lista-menu-6').css("display", "none");
-	$('#lista-menu-7').css("display", "none");
 });
 
 $('#clavicula').click(function(){
